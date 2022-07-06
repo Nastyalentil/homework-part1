8aadba4f700e77661a7262952a2d2d2bf49319a6
Nastyalentil <103311559+Nastyalentil@users.noreply.github.com>
2022-07-06 23:09:05 +0300

    Add files via upload

--- дз11.py
+++ дз11.py
@@ -1,14 +1,32 @@
-print('Вітаю! Це шифрувальник для шифру Цезаря.\n(Алфавіт - англійський)')
+print('Вітаю! Це шифрувальник та дешифрувальник для шифру Цезаря.\n(Алфавіт - англійський)')
 
 alphabet = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'
-text = input('Введіть текст для шифрування: ')
-key = int(input('Введіть ключ від 1 до 25: '))
-text2 = ''
-for letter in text:
-    position = alphabet.find(letter)
-    newposition = position + key
-    if letter in alphabet:
-        text2 = text2 + alphabet[newposition]
-    else:
-        text2 = text2 + letter
-print(text2)
+
+choice = int(input('Введіть "1" для шифрування або "2" для дешифрування: '))
+
+if choice == 1:
+    text = input('Введіть текст для шифрування: ')
+    key = int(input('Введіть ключ від 1 до 25: '))
+    text = text.lower()
+    text2 = ''
+    for letter in text:
+        position = alphabet.find(letter)
+        newposition = position + key
+        if letter in alphabet:
+            text2 = text2 + alphabet[newposition]
+        else:
+            text2 = text2 + letter
+    print(text2)
+elif choice == 2:
+    text = input('Введіть текст для шифрування: ')
+    key = int(input('Введіть ключ від 1 до 25: '))
+    text = text.lower()
+    text2 = ''
+    for letter in text:
+        position = alphabet.find(letter)
+        newposition = position - key
+        if letter in alphabet:
+            text2 = text2 + alphabet[newposition]
+        else:
+            text2 = text2 + letter
+    print(text2)
